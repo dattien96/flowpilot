72dbacdd7db6c970e14a1858e476b5111014c7e1
DatNguyen <nguyenhotiendat.it@gmail.com>
2026-05-25 14:44:00 +0700

    [Refactor]: Remove legacy feature module domain logic and pages

--- apps/admin-web/src/domain/usecase/features/list-features-usecase.ts
+++
@@ -1,9 +0,0 @@
-import type { FeatureGateway } from "@/domain/gateway/feature-gateway";
-
-export class ListFeaturesUseCase {
-  constructor(private readonly featureGateway: FeatureGateway) {}
-
-  execute() {
-    return this.featureGateway.listFeatures();
-  }
-}
